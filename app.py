dee1f102c8665991d36a2d48aaa3e5ea4f73b1b0
Wolfan <fomoro@gmail.com>
2025-12-02 20:21:48 -0500

    agregamos un template y modificamos el puerto para subirlo a render

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def hola():
+    return render_template("holaflask.html")
+
 
 @app.get("/")
 def home():
@@ -15,4 +20,4 @@ def recibir_mensaje():
     }
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0',port=80,debug=True)
